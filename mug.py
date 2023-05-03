18de4c20ca2be29b8fd769e3b66f7242c5e6b1ad
gabicfa <gabrielacfalmeida@gmail.com>
2023-05-03 18:45:11 +0100

    Main part of mug modelled

--- mug.py
+++ mug.py
@@ -4,21 +4,130 @@
 import prman
 
 ri = prman.Ri()  # create an instance of the RenderMan interface
+ri.Option("rib", {"string asciistyle": "indented"})
 
-filename = "__render"  # "mug.rib"
+filename = "mug.rib"
 
 ri.Begin("__render")
 ri.Display("mug.exr", "it", "rgba")
 # Specify PAL resolution 1:1 pixel Aspect ratio
 ri.Format(720, 576, 1)
-ri.Projection(ri.PERSPECTIVE)
+ri.Projection(ri.PERSPECTIVE, {ri.FOV: 50})
+
+# setup the raytrace / integrators
+ri.Hider("raytrace", {"int incremental": [1]})
+ri.ShadingRate(10)
+ri.PixelVariance(0.1)
+ri.Integrator("PxrPathTracer", "integrator", {})
+ri.Option("statistics", {"filename": ["stats.txt"]})
+ri.Option("statistics", {"endofframe": [1]})
+ri.Option("searchpath", {"string texture": "./textures/:@"})
+
 
 # now we start our world
 ri.WorldBegin()
-ri.Translate(0, -6, 15)
-ri.Rotate(-90, 1, 0, 0)
-ri.Cylinder(4, 4.5, 12, 360)
-ri.Cylinder(4, 0.5, 4.5, 360)
+
+# create a simple checker pattern
+expr = """
+$colour = c1;
+$c = floor( 10 * $u ) +floor( 10 * $v );
+if( fmod( $c, 2.0 ) < 1.0 )
+{
+	$colour=c2;
+}
+$colour
+"""
+
+ #######################################################################
+# Lighting We need geo to emit light
+#######################################################################
+ri.TransformBegin()
+ri.AttributeBegin()
+ri.Declare("domeLight", "string")
+ri.Rotate(45, 0, 1, 0)
+ri.Rotate(90, 1, 0, 0)
+ri.Rotate(100, 0, 0, 1)
+ri.Attribute("visibility", {"int camera": [0]})
+ri.Light("PxrDomeLight", "domeLight", {"string lightColorMap": "Luxo-Jr_4000x2000.tex"})
+ri.AttributeEnd()
+ri.TransformEnd()
+#######################################################################
+# end lighting
+#######################################################################
+
+ri.Translate(0, -5.5, 30)
+ri.Rotate(-110, 1, 0, 0)
+
+ri.TransformBegin()
+ri.Attribute("identifier", {"name": "top"})
+# use the pattern
+ri.Pattern("PxrSeExpr", "seTexture", {"color c1": [1, 1, 1], "color c2": [1, 0, 0], "string expression": [expr]})
+ri.Bxdf(
+    "PxrDiffuse",
+    "diffuse",
+    {
+        #  'color diffuseColor'  : [1,0,0]
+        "reference color diffuseColor": ["seTexture:resultRGB"]
+    },
+)
+
+ri.Cylinder(5, 4.5, 12, 360)
+ri.Attribute("identifier", {"name": "inside"})
+# ri.Bxdf("PxrDiffuse", "bxdf", {"color diffuseColor": [0, 1, 0]})
+ri.Cylinder(4.85, 0.5, 11.95, 360)
+
+ri.Attribute("identifier", {"name": "bottom"})
+# use the pattern
+ri.Pattern("PxrSeExpr", "seTexture", {"color c1": [1, 1, 1], "color c2": [0, 0, 1], "string expression": [expr]})
+ri.Bxdf(
+    "PxrDiffuse",
+    "diffuse",
+    {
+        #  'color diffuseColor'  : [1,0,0]
+        "reference color diffuseColor": ["seTexture:resultRGB"]
+    },
+)
+ri.Cylinder(5.05, 0.5, 4.5, 360)
+ri.TransformEnd()
+
+ri.TransformBegin()
+ri.Translate(0, 0, 11.95)
+ri.Attribute("identifier", {"name": "inside_top_edge"})
+ri.Bxdf("PxrDiffuse", "bxdf", {"color diffuseColor": [1, 0, 0]})
+ri.Torus(4.9, 0.1, 0, 180, 360)
+ri.TransformEnd()
+
+ri.TransformBegin()
+ri.Translate(0, 0, 4.424)
+ri.Attribute("identifier", {"name": "middle_texture"})
+ri.Bxdf("PxrDiffuse", "bxdf", {"color diffuseColor": [0, 0, 1]})
+ri.Torus(5, 0.13, 0, 180, 360)
+ri.TransformEnd()
+
+ri.TransformBegin()
+ri.Bxdf("PxrDiffuse", "bxdf", {"color diffuseColor": [0, 1, 1]})
+points = [0, -10, 30, -1, -0.5, 1, 2, 0.5, 1, 1, 0, -1]
+width = [1, 4]
+# ri.Curves("cubic", [4], "nonperiodic", {ri.P: points, ri.WIDTH: width})
+ri.TransformEnd()
+
+ri.TransformBegin()
+ri.Translate(0, 0, -4.31)
+ri.Pattern("PxrSeExpr", "seTexture", {"color c1": [1, 1, 1], "color c2": [0, 0, 1], "string expression": [expr]})
+ri.Bxdf(
+    "PxrDiffuse",
+    "diffuse",
+    {
+        #  'color diffuseColor'  : [1,0,0]
+        "reference color diffuseColor": ["seTexture:resultRGB"]
+    },
+)
+ri.Paraboloid(5.06, 4.8, 4.0, 360)
+# ri.Bxdf("PxrDiffuse", "bxdf", {"color diffuseColor": [0, 1, 1]})
+ri.Disk(4, 4.6, 360)
+ri.TransformEnd()
+
 ri.WorldEnd()
+
 # and finally end the rib file
 ri.End()
